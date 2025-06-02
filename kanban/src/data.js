e315772ccd462e3a0fced72df80731fbbf960cc9
TrikMary <shanmarishka@gmail.com>
2025-06-02 20:25:58 +0300

    создала список карточек

---
+++ kanban/src/data.js
@@ -0,0 +1,44 @@
+export const cardList = [
+    {
+        id: 1,
+        topic: "Web Design",
+        title: "Доделать домашку",
+        date: "30.10.23",
+        status: "Без статуса",
+    },
+    {
+        id: 2,
+        topic: "Research",
+        title: "Написать список",
+        date: "31.11.21",
+        status: "Без статуса",
+    },
+    {
+        id: 3,
+        topic: "Copywriting",
+        title: "Внести счетчики",
+        date: "12.12.24",
+        status: "Нужно сделать",
+    },
+    {
+        id: 4,
+        topic: "Copywriting",
+        title: "Найти схему",
+        date: "05.04.25",
+        status: "В работе",
+    },
+    {
+        id: 5,
+        topic: "Research",
+        title: "Доделать проект",
+        date: "03.03.25",
+        status: "Тестирование",
+    },
+    {
+        id: 6,
+        topic: "Web Design",
+        title: "Найти задание",
+        date: "01.01.25",
+        status: "Готово",
+    }
+]
