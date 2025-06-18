907fff8d5fcae682e3ab6e5c218ac55da94bcaac
TrikMary <shanmarishka@gmail.com>
2025-06-18 20:19:52 +0300

    закончила стилизацию PopExit

--- kanban/src/components/PopExit/PopExit.jsx
+++ kanban/src/components/PopExit/PopExit.jsx
@@ -1,26 +1,26 @@
 
-
+import * as S from "./popExit.styled.js"
 
 export function PopExit () {
     return (
-        <div className="pop-exit" id="popExit">
-        <div className="pop-exit__container">
-          <div className="pop-exit__block">
-            <div className="pop-exit__ttl">
+        <S.PopExit id="popExit">
+        <S.PopExitContainer>
+          <S.PopExitBlock>
+            <S.PopExitTitle>
               <h2>Выйти из аккаунта?</h2>
-            </div>
-            <form className="pop-exit__form" id="formExit" action="#">
-              <div className="pop-exit__form-group">
-                <button className="pop-exit__exit-yes _hover01" id="exitYes">
+            </S.PopExitTitle>
+            <S.PopExitForm id="formExit" action="#">
+              <S.PopExitFormGroup>
+                <S.ButtonPopExitYes id="exitYes">
                   <a href="modal/signin.html">Да, выйти</a>{" "}
-                </button>
-                <button className="pop-exit__exit-no _hover03" id="exitNo">
+                </S.ButtonPopExitYes>
+                <S.ButtonPopExitNo id="exitNo">
                   <a href="main.html">Нет, остаться</a>{" "}
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+                </S.ButtonPopExitNo>
+              </S.PopExitFormGroup>
+            </S.PopExitForm>
+          </S.PopExitBlock>
+        </S.PopExitContainer>
+      </S.PopExit>
     );
 }
