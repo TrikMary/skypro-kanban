5b9e248df3e8310fb4b4147a46b5468eb9bfe17d
TrikMary <shanmarishka@gmail.com>
2025-06-25 15:57:26 +0300

    стилизованны карточки кроме цветных тем

--- kanban/src/components/Card/Card.jsx
+++ kanban/src/components/Card/Card.jsx
@@ -1,10 +1,10 @@
-
+import * as S from "./Card.styled.js"
 
 export function Card ({ topic, title, date }) {
     return (
-        <div className="cards__item">
-        <div className="cards__card card">
-          <div className="card__group">
+        <S.CardsItem>
+        <S.CardsCard>
+          <S.CardGroup>
             <div className="card__theme _orange">
               <p className="_orange">{topic}</p>
             </div>
@@ -15,13 +15,13 @@ export function Card ({ topic, title, date }) {
                 <div />
               </div>
             </a>
-          </div>
-          <div className="card__content">
+          </S.CardGroup>
+          <S.CardContent>
             <a href="" target="_blank">
-              <h3 className="card__title">{title}</h3>
+              <S.CardTitle>{title}</S.CardTitle>
               
             </a>
-            <div className="card__date">
+            <S.CardDate>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width={13}
@@ -52,9 +52,9 @@ export function Card ({ topic, title, date }) {
               </svg>
               
               <p>{date}</p>
-            </div>
-          </div>
-        </div>
-      </div>
+            </S.CardDate>
+          </S.CardContent>
+        </S.CardsCard>
+      </S.CardsItem>
     );
 }
---
+++ kanban/src/components/Card/Card.styled.js
@@ -0,0 +1,71 @@
+import styled from "styled-components"
+
+export const CardsItem = styled.div`
+  padding: 5px;
+  animation-name: card-animation;
+  animation-duration: 500ms;
+  animation-timing-function: linear;
+`
+
+export const CardsCard = styled.div`
+  width: 220px;
+  height: 130px;
+  background-color: #FFFFFF;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: stretch;
+  padding: 15px 13px 19px;
+
+  width: 100%;
+  display: block;
+  position: relative;
+`
+
+export const CardGroup = styled.div`
+  width: 100%;
+  height: 20px;
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const CardTheme = styled.div`
+
+`
+
+export const CardContent = styled.div`
+  height: 64px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+`
+
+export const CardTitle = styled.h3`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 18px;
+  color: #000000;
+  margin-bottom: 10px;
+`
+
+export const CardDate = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  svg {
+  width: 13px;
+}
+
+p {
+  margin-left: 6px;
+  font-size: 10px;
+  line-height: 13px;
+  color: #94A6BE;
+  letter-spacing: 0.2px;
+}
+`
